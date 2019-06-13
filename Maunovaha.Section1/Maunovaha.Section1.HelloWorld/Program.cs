88bf2637b41bbf376bcb7378760ef2197842f48b
Mauno Vähä <maunovaha@gmail.com>
2019-06-14 00:02:57 +0300

    Hello World

---
+++ Maunovaha.Section1/Maunovaha.Section1.HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Maunovaha.Section1.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Stranger";
+
+            Console.WriteLine($"Hello {name}!");
+
+            // Requires <Enter> to exit the program
+            Console.Read();
+        }
+    }
+}
