95f4a56a86c99e6569d3e617d9694604090cfcf9
Mauno Vähä <maunovaha@gmail.com>
2019-06-14 18:42:35 +0300

    Modifies Hello World program to change console colors

--- Maunovaha.Section1/Maunovaha.Section1.HelloWorld/Program.cs
+++ Maunovaha.Section1/Maunovaha.Section1.HelloWorld/Program.cs
@@ -4,13 +4,22 @@ namespace Maunovaha.Section1.HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            var name = "Stranger";
+            // Sets console text color
+            Console.ForegroundColor = ConsoleColor.Green;
 
+            // Sets console background color
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+
+            // Sets the whole console window to use the defined colors 
+            // (fills the background as well)
+            Console.Clear();                                                                       
+
+            string name = "Stranger";
             Console.WriteLine($"Hello {name}!");
 
-            // Requires <Enter> to exit the program
+            // Requires <Enter> to exit the program (only on Windows)
             Console.Read();
         }
     }
